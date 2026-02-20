38102ab41bdec6f721e0a8e414d91625ccab942e
Rohit Kalyan Kandulapati <86190738+rohit-kandulapati@users.noreply.github.com>
2026-02-20 23:29:18 +0000

    CORS settings commented

--- api/main.py
+++ api/main.py
@@ -21,13 +21,13 @@ origins = [
     "*"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # Pydantic model for request validation
 class QRRequest(BaseModel):
