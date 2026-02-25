5f6860c84af765e02e45c00470dc6b08f71b8a28
Rohit Kalyan K <mail.rohitkalyan@gmail.com>
2026-02-25 23:35:11 +0530

    irsa terraform added

--- terraform/s3-irsa-variables.tf
+++ terraform/s3-irsa-variables.tf
@@ -1,8 +1,26 @@
 locals {
-  oidc-issuer = module.eks.cluster_name.identity[0].oidc[0].list
+  oidc_issuer = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_iam_openid_connect_provider" "oidc_arn" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
 
 variable "bucket" {
   type = string
   default = "qrcode-rohit-kalyan"
+}
+
+variable "namespace" {
+  type = string
+  default = "default"
+}
+
+variable "sa_name" {
+  type = string
+  default = "s3-getput"
 }
--- terraform/s3-irsa.tf
+++ terraform/s3-irsa.tf
@@ -11,6 +11,42 @@ resource "aws_s3_bucket_public_access_block" "bucket-block" {
   restrict_public_buckets = true
 }
 
+resource "aws_iam_role" "s3-role" {
+  name = "irsa-s3-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = data.aws_iam_openid_connect_provider.oidc_arn.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${local.oidc_issuer}:sub" = "system:serviceaccount:${var.namespace}:${var.sa_name}" ## subject  
+          "${local.oidc_issuer}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
 
-
+resource "aws_iam_role_policy" "default" {
+  name   = "eks-irsa-policy"
+  role   = aws_iam_role.s3-role.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "Statement1",
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        "Resource": "arn:aws:s3:::${var.bucket}/*"
+      }
+    ]
+  })
+}
